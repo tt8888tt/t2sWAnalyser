6084f46404d6fb9c69e7b173407f247db4f9b757
dajeong jeon <xicuvybgh@gmail.com>
2018-05-24 19:23:19 +0900

    Update a.cc

--- t2sWAnalyser/plugins/a.cc
+++ t2sWAnalyser/plugins/a.cc
@@ -5,15 +5,15 @@ std::vector<std::pair<int, int> > GEMSimpleModel::simulateClustering(const GEMEt
   const LocalPoint& hit_entry(simHit->entryPoint());
   const LocalPoint& hit_exit(simHit->exitPoint());
 
-  float hit_entry_smeardX = hit_entry.x()-fabs(CLHEP::RandGaussQ::shoot(0, 0.1));
-  float hit_exit_smeardX = hit_exit.x()+fabs(CLHEP::RandGaussQ::shoot(0, 0.1));
+  float hit_entry_smearedX = hit_entry.x()-fabs(CLHEP::RandGaussQ::shoot(0, 0.1));
+  float hit_exit_smearedX = hit_exit.x()+fabs(CLHEP::RandGaussQ::shoot(0, 0.1));
   if (hit_entry.x()>hit_exit.x()) {
-    hit_entry_smeardX = hit_entry.x()+fabs(CLHEP::RandGaussQ::shoot(0, 0.1));
-    hit_exit_smeardX = hit_exit.x()-fabs(CLHEP::RandGaussQ::shoot(0, 0.1));
+    hit_entry_smearedX = hit_entry.x()+fabs(CLHEP::RandGaussQ::shoot(0, 0.1));
+    hit_exit_smearedX = hit_exit.x()-fabs(CLHEP::RandGaussQ::shoot(0, 0.1));
   }
 
-  LocalPoint inPoint(hit_entry_smeardX, hit_entry.y(), hit_entry.z());
-  LocalPoint outPoint(hit_exit_smeardX, hit_exit.y(), hit_exit.z());
+  LocalPoint inPoint(hit_entry_smearedX, hit_entry.y(), hit_entry.z());
+  LocalPoint outPoint(hit_exit_smearedX, hit_exit.y(), hit_exit.z());
 
   int clusterStart = roll->strip(inPoint);
   int clusterEnd = roll->strip(outPoint);
